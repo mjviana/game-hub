20d3e37fed946aac1d5910373b7c5c83202b2a60
Mário Viana <mj.viana1993@hotmail.com>
2023-03-20 22:05:49 +0000

    Filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,12 +1,18 @@
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import { Genre } from '../hooks/useGenres';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import GameCardSkeleton from './GameCardSkeleton';
 
-export default function GameGrid()
+interface Props
 {
-  const { data, error, isLoading } = useGames();
+  selectedGenre: Genre | null
+}
+
+export default function GameGrid({ selectedGenre }: Props)
+{
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,4 +1,4 @@
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
@@ -8,7 +8,7 @@ interface FetchResponse<T>
     results: T[]
 }
 
-const useData = <T>(endpoint: string) =>
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) =>
 {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState('');
@@ -19,7 +19,7 @@ const useData = <T>(endpoint: string) =>
         const controller = new AbortController();
 
         setIsLoading(true)
-        apiClient.get<FetchResponse<T>>(`${endpoint}`, { signal: controller.signal })
+        apiClient.get<FetchResponse<T>>(`${endpoint}`, { signal: controller.signal, ...requestConfig })
             .then(res =>
             {
                 setData(res.data.results)
@@ -33,7 +33,7 @@ const useData = <T>(endpoint: string) =>
             });
 
         return () => controller.abort();
-    }, []);
+    }, deps ? [...deps] : []);
 
     return { data, error, isLoading }
 
