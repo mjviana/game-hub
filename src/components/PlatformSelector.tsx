9cb3a176923abdbb27ff0bf1fd41bc70da8e81c0
Mário Viana <mj.viana1993@hotmail.com>
2023-04-29 16:42:52 +0100

    Add react-query to fetch platforms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,7 +1,7 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { BsChevronDown } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
-import usePlatforms from "../hooks/usePlatforms";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+import { Platform } from '../hooks/useGames';
+import usePlatforms from '../hooks/usePlatforms';
 
 interface Props {
   onSelectedPlatform(platform: Platform): void;
@@ -9,17 +9,17 @@ interface Props {
 }
 
 const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = usePlatforms();
+  const { data, error } = usePlatforms();
 
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatform?.name || "Platforms"}
+        {selectedPlatform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem onClick={() => onSelectedPlatform(platform)} key={platform.id}>
             {platform.name}
           </MenuItem>
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import useData from './useData';
+import { useQuery } from '@tanstack/react-query';
+import platforms from '../data/platforms';
+import apiClient from '../services/api-client';
+import useData, { FetchResponse } from './useData';
 
 interface Platform {
   id: number;
@@ -6,6 +9,18 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('platforms/lists/parents');
+const fetchPlatforms = () => {
+  return apiClient
+    .get<FetchResponse<Platform>>('platforms/lists/parents')
+    .then((res) => res.data);
+};
+
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ['platforms'],
+    queryFn: fetchPlatforms,
+    staleTime: 24 * 60 * 60 * 1000, // 1 day
+    initialData: { count: platforms.length, results: platforms }
+  });
 
 export default usePlatforms;
