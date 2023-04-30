3b45872db24861fbcf4dd72785b7ee52a9579512
Mário Viana <mj.viana1993@hotmail.com>
2023-04-30 16:24:14 +0100

    Refactoring - removed useData hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,7 @@
 // eslint-disable-next-line import/no-cycle
 import { useQuery } from '@tanstack/react-query';
 import { GameQuery } from '../App';
-import apiClient from '../services/api-client';
-import { FetchResponse } from './useData';
+import apiClient, { FetchResponse } from '../services/api-client';
 
 export interface Platform {
   id: number;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
-  params: { key: 'c66bcb42cb3d436faf5d6b672affa64d' },
+  params: { key: 'c66bcb42cb3d436faf5d6b672affa64d' }
 });
